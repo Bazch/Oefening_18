24fc995a694466eb43b6c18aeeff2fa41b4ae941
Bazch <doppleb@gmail.com>
2019-10-22 14:58:33 +0200

    Oefening 6

---
+++ src/JavaTrader.java
@@ -0,0 +1,14 @@
+public class JavaTrader {
+
+    public static void main(String[] args) {
+        Aandelen amazon = new Aandelen("AMZN");
+        Aandelen oracle = new Aandelen("ORCL", 100);
+        amazon.koop(100);
+        oracle.koop(200);
+        oracle.koop(200);
+
+        System.out.println(amazon.toString());
+        System.out.println(oracle.toString());
+
+    }
+}
